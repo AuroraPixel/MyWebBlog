5a8f03e66513ff66775aaa1e31c3ce25eeb4a57b
wanghanlin <hanlin.wang@raipiot.com>
2023-05-15 15:52:15 +0800

    Flowing Fonts

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import HeaderTyper from '../../components/HeaderTyper'
+import style from './styles.module.css'
+import { shuffle } from '../../utils'
+
+const index = () => {
+    const [toTypeWords] = useState(
+        shuffle([
+          'Email:wanghanlinlin@126.com',
+          'QQ:894606483',
+          'WeiChat:wang894606483',
+          'AuroraPiexl',
+        ])
+      );
+
+    return (
+        <div>
+            <HeaderTyper
+                className={style.HeaderTyper}
+                words={toTypeWords}
+                delay={5000}
+                defaultText={toTypeWords[0] || 'Aurora'}
+            />
+        </div>
+    )
+}
+
+export default index
